8c547db99cf27623a28f3c2ec2fbe74fc0117138
zulfikarauliaf <captn.zulfikar@gmail.com>
2022-09-12 22:24:04 +0700

    tambah getAll

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./connection');
 exports.index = function(req,res){
     response.ok("REST API Running",res)
 };
+
+//menampilkan data product
+exports.getAllProduct = function(req,res){
+    connection.query('SELECT * FROM product', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    });
+};
--- routes.js
+++ routes.js
@@ -5,4 +5,7 @@ module.exports = function(app) {
 
     app.route('/')
         .get(jsonku.index);
+
+    app.route('/show')
+        .get(jsonku.getAllProduct);
 }
