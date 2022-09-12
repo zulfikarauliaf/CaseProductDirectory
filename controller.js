9e508e3a9d825dc3f2fa288088f3c29be3d5e9b5
zulfikarauliaf <captn.zulfikar@gmail.com>
2022-09-12 22:06:35 +0700

    fix controller

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./connection');
 
 exports.index = function(req,res){
-    response.ok("REST API Running")
+    response.ok("REST API Running",res)
 };
